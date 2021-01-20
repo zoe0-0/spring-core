441e8ffdb26e421216ef5ddd95f1d0fb296e7ce4
zoe0-0 <duohui1234@gmail.com>
2021-01-20 18:29:48 +0900

    Test MemberService using junit

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,4 +1,23 @@
 package hello.core.member;
 
-public class MemberServiceTest {
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest  {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L,"memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+
+    }
 }
