54bc09156269d00652b06544edd24e0111342cae
zoe0-0 <duohui1234@gmail.com>
2021-02-06 00:12:30 +0900

    Refator code using spring container

--- src/main/java/hello/core/discount/DiscountPolicy.java
+++ src/main/java/hello/core/discount/DiscountPolicy.java
@@ -3,6 +3,7 @@ package hello.core.discount;
 import hello.core.member.Member;
 
 public interface DiscountPolicy {
+
     //@return 할인금액
     int discount(Member member,int price);
 }
---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,44 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+        }
+
+    }
+
+
+
+    @Test
+    @DisplayName("에플리케이션 빈 출력하기")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){  //내가 개발하기 위해 등록한 빈
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+
+    }
+
+
+
+
+}
